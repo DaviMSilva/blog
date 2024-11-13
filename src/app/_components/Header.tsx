6be0ee38ea3cdc1237881a61304ae6eb117901fb
Davi Silva <davisilvameneses@gmail.com>
2024-11-13 01:06:49 -0300

    wip

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,29 @@
+'use client'
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+export const Header = () => {
+    const path = usePathname();
+
+    const isHome = path === '/'
+
+    return (
+        <header className="w-screen">
+            <nav className="flex justify-between items-center mx-auto h-14  max-w-lg">
+                <a href="/" className="font-semibold text-xl group">
+                    DaviMSilva
+                    <div data-is-path={isHome} className="block w-5 bg-blue-400 h-1 group-hover:w-full data-[is-path=true]:w-full  transition-all">
+                    </div>
+                </a>
+
+                <div>
+                    <Link href={'/about'} className="font-medium group">
+                        About
+                        <div data-is-path={path === '/about'} className="block w-5 bg-blue-400 h-1 group-hover:w-full data-[is-path=true]:w-full  transition-all">
+                        </div>
+                    </Link>
+                </div>
+            </nav>
+        </header>
+    )
+}
---
+++ src/app/about/page.tsx
@@ -0,0 +1,35 @@
+export default function About () {
+    return (
+        <>
+            <article className="mb-3 border-b">
+                <img className="max-w-28 rounded-full border-gray-400 border-4" src="https://media.licdn.com/dms/image/v2/D4D03AQEC7AgcsYdnVw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1720118331398?e=1736985600&v=beta&t=pTX3pVMDGwi6FpAQTjpMaSHL4q_DrxpomTNj4uUuiac" alt="" />
+                <div className="font-medium text-lg mt-2">
+                    Davi Silva
+                </div>
+                <div className="text-sm font-semibold text-blue-400">
+                    Software Developer
+                </div>
+                <p className="mt-5">
+                    I'm a Brazilian born in the state of Ceará, not far from the ocean, as a child I always liked logic questions and video games.
+                </p>
+                <p>
+                    At school I played chess and participated in mathematics and astronomy Olympiads, I always studied in public schools.
+                </p>
+                <p>
+                    In high school, I went to a full-time school where I took a technical course in finance, I believed that I would work in this area in the future. But I fell in love with programming.
+                </p>
+                <p>
+                    In 2019 I started my bachelor's degree in computer science at the <a href="https://pt.wikipedia.org/wiki/Instituto_Federal_de_Educa%C3%A7%C3%A3o,_Ci%C3%AAncia_e_Tecnologia_do_Cear%C3%A1">federal institute of ceara</a>.
+                    In the middle of college, I focused on learning to program with modern languages ​​and frameworks to get a job.
+                </p>
+                <p>
+                    In 2023 I got a job, working as a fullstack developer, where I had contact and carried out various tasks using PHP Laravel, VueJs and also developed an application for the company using React Native.
+                </p>
+                <p>        
+                    In the middle of 2024 I finished college and continue to seek more knowledge and challenges in my career as a developer
+                </p>
+
+            </article>
+        </>
+    )
+}
